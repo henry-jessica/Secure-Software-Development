76f61f59f1d133e4c2a7059e1d6df8a5967d1a5f
Jessica Henry <s00214115@atu.ie>
2023-12-29 22:42:25 +0000

    refactory handler encryp & decry class

--- SSD Assignment - Banking Application/AesEncryptionHendler.cs
+++ SSD Assignment - Banking Application/AesEncryptionHendler.cs
@@ -1,83 +1,125 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.IO;
-using System.Threading.Tasks;
 using System.IO;
 using System.Security.Cryptography;
-
+using System.Text;
 
 namespace Banking_Application
 {
-    public static class AesEncryptionHendler
+    public static class AesEncryptionHandler
     {
-        public static Aes GetOrCreateAesEncryptionKey(byte[] iv)
+        private const string CryptoKeyName = "bank_crypto_key_name";
+        private static readonly CngProvider KeyStorageProvider = CngProvider.MicrosoftSoftwareKeyStorageProvider;
+
+        public static Aes GetOrCreateAesEncryptionKeyCBC(byte[] iv)
         {
-            RNGCryptoServiceProvider rng = new RNGCryptoServiceProvider();
-
-            String crypto_key_name = "bank_crypto_key_name";
-            CngProvider key_storage_provider = CngProvider.MicrosoftSoftwareKeyStorageProvider;
-
-
-            if (!CngKey.Exists(crypto_key_name, key_storage_provider))
+            if (!CngKey.Exists(CryptoKeyName, KeyStorageProvider))
             {
-
-                CngKeyCreationParameters key_creation_parameters = new CngKeyCreationParameters()
-                {
-                    Provider = key_storage_provider
-                };
-
-                CngKey.Create(new CngAlgorithm("AES"), crypto_key_name, key_creation_parameters);
-
+                CreateAesKey();
             }
-            Aes aes = aes = new AesCng(crypto_key_name, key_storage_provider); // Declare aes outside the if block
 
-            aes.Mode = CipherMode.CBC;
-            aes.IV = iv;
-            aes.Padding = PaddingMode.PKCS7;
+            Aes aes = new AesCng(CryptoKeyName, KeyStorageProvider);
+
+            ConfigureAesParameters(aes, iv, CipherMode.CBC);
 
             return aes;
         }
 
-        public static byte[] Encrypt(byte[] plaintext_data, Aes aes)
+        public static Aes GetOrCreateAesEncryptionKeyECB()
         {
+            if (!CngKey.Exists(CryptoKeyName, KeyStorageProvider))
+            {
+                CreateAesKey();
+            }
 
-            byte[] ciphertext_data;//Byte Array Where Result Of Encryption Operation Will Be Stored.
+            Aes aes = new AesCng(CryptoKeyName, KeyStorageProvider);
 
-            ICryptoTransform encryptor = aes.CreateEncryptor();//Object That Contains The AES Encryption Algorithm (Using The Key and IV Value Specified In The AES Object). 
+            ConfigureAesParameters(aes, null, CipherMode.ECB);
 
-            MemoryStream msEncrypt = new MemoryStream();//MemoryStream That Will Store The Output Of The Encryption Operation.
-
-            CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
-            csEncrypt.Write(plaintext_data, 0, plaintext_data.Length);//Writes All Data Contained In plaintext_data Byte Array To CryptoStream (Which Then Gets Encrypted And Gets Written to the msEncrypt MemoryStream).
-            csEncrypt.Dispose();//Closes CryptoStream
-
-            ciphertext_data = msEncrypt.ToArray();//Output Result Of Encryption Operation In Byte Array Form.
-            msEncrypt.Dispose();//Closes MemoryStream
-
-            return ciphertext_data;
-
+            return aes;
         }
 
-        public static byte[] Decrypt(byte[] ciphertext_data, Aes aes)
+        private static void CreateAesKey()
         {
+            CngKeyCreationParameters keyCreationParameters = new CngKeyCreationParameters
+            {
+                Provider = KeyStorageProvider
+            };
 
-            byte[] plaintext_data;//Byte Array Where Result Of Decryption Operation Will Be Stored.
+            CngKey.Create(new CngAlgorithm("AES"), CryptoKeyName, keyCreationParameters);
+        }
 
-            ICryptoTransform decryptor = aes.CreateDecryptor();//Object That Contains The AES Decryption Algorithm (Using The Key and IV Value Specified In The AES Object). 
+        private static void ConfigureAesParameters(Aes aes, byte[] iv, CipherMode mode)
+        {
+            aes.Mode = mode;
 
-            MemoryStream msDecrypt = new MemoryStream();//MemoryStream That Will Store The Output Of The Decryption Operation.
+            if (iv != null)
+            {
+                aes.IV = iv;
+            }
 
-            CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write);//Writes All Data Contained In Byte Array To CryptoStream (Which Then Gets Decrypted).
-            csDecrypt.Write(ciphertext_data, 0, ciphertext_data.Length);//Writes All Data Contained In ciphertext_data Byte Array To CryptoStream (Which Then Gets Decrypted And Gets Written to the msDecrypt MemoryStream).
-            csDecrypt.Dispose();//Closes CryptoStream
+            aes.Padding = PaddingMode.PKCS7;
+        }
 
-            plaintext_data = msDecrypt.ToArray();//Output Result Of Decryption Operation In Byte Array Form.
-            msDecrypt.Dispose();//Closes MemoryStream
+        public static byte[] Encrypt(byte[] plaintextData, Aes aes)
+        {
+            byte[] ciphertextData;
 
-            return plaintext_data;
+            using (ICryptoTransform encryptor = aes.CreateEncryptor())
+            {
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csEncrypt.Write(plaintextData, 0, plaintextData.Length);
+                    }
 
+                    ciphertextData = msEncrypt.ToArray();
+                }
+            }
+
+            return ciphertextData;
+        }
+
+        public static byte[] Decrypt(byte[] ciphertextData, Aes aes)
+        {
+            byte[] plaintextData;
+
+            using (ICryptoTransform decryptor = aes.CreateDecryptor())
+            {
+                using (MemoryStream msDecrypt = new MemoryStream())
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Write))
+                    {
+                        csDecrypt.Write(ciphertextData, 0, ciphertextData.Length);
+                    }
+
+                    plaintextData = msDecrypt.ToArray();
+                }
+            }
+
+            return plaintextData;
+        }
+
+        public static string EncryptAccountNumber(string text)
+        {
+            Aes aes = GetOrCreateAesEncryptionKeyECB();
+            byte[] plaintextData = Encoding.ASCII.GetBytes(text);
+            byte[] ciphertextData = Encrypt(plaintextData, aes);
+
+            aes.Dispose();
+
+            return Convert.ToBase64String(ciphertextData);
+        }
+
+        public static string DecryptAccountNumber(string text)
+        {
+            Aes aes = GetOrCreateAesEncryptionKeyECB();
+            byte[] ciphertextData = Convert.FromBase64String(text);
+            byte[] plaintextData = Decrypt(ciphertextData, aes);
+
+            aes.Dispose();
+
+            return Encoding.ASCII.GetString(plaintextData);
         }
     }
 }
