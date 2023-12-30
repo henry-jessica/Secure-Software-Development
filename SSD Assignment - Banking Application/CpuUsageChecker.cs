3961779e4eba335098e7773e8b0c9669783b3f7b
Jessica Henry <s00214115@atu.ie>
2023-12-30 20:00:59 +0000

    Get CPU usage and checker added before each task

---
+++ SSD Assignment - Banking Application/CpuUsageChecker.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Application
+{
+    public class CpuUsageChecker
+    {
+        private PerformanceCounter cpuCounter;
+
+        public CpuUsageChecker()
+        {
+            // Initialize the CPU counter
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+        }
+
+        public float GetCpuUsage()
+        {
+            // Get the current CPU usage
+            float cpuUsage = cpuCounter.NextValue();
+            System.Threading.Thread.Sleep(1000);
+            // Now matches task manager reading
+            cpuUsage = cpuCounter.NextValue();
+            return cpuUsage;
+        }
+
+    }
+}
