4e98e98c239f8e772e30895a0198b374fe3a30f4
Jessica Henry <s00214115@atu.ie>
2024-01-01 21:55:40 +0000

    cerate exception message

---
+++ SSD Assignment - Banking Application/DataIntegrityException.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SSD_Assignment___Banking_Application
+{
+    public class DataIntegrityException : Exception
+    {
+        public DataIntegrityException() { }
+
+        public DataIntegrityException(string message) : base(message) { }
+
+        public DataIntegrityException(string message, Exception innerException) : base(message, innerException) { }
+    }
+}
