099830890efc873be99afb1421f6e9a1e1d347a7
Jessica Henry <s00214115@atu.ie>
2024-01-02 00:03:24 +0000

    program refactory

--- SSD Assignment - Banking Application/Program.cs
+++ SSD Assignment - Banking Application/Program.cs
@@ -24,6 +24,7 @@ namespace Banking_Application
 
             Data_Access_Layer dal = Data_Access_Layer.getInstance();
             bool running = true;
+            Bank_Account ba = null; 
 
             do
             {
@@ -43,193 +44,17 @@ namespace Banking_Application
 
                 else
                 {
-                    Console.WriteLine("");
-                    Console.WriteLine("***Banking Application Menu***");
-                    Console.WriteLine("1. Add Bank Account");
-                    Console.WriteLine("2. Close Bank Account");
-                    Console.WriteLine("3. View Account Information");
-                    Console.WriteLine("4. Make Lodgement");
-                    Console.WriteLine("5. Make Withdrawal");
-                    Console.WriteLine("6. Exit");
-                    Console.WriteLine("CHOOSE OPTION:");
-                    String option = Console.ReadLine();
+                    string option = GetMenu();
 
                     switch (option)
                     {
                         case "1":
-                            int loopCount = 0;
-                            string accountType = ""; 
-                            const string SENTINEL = "3"; 
 
-                            do
+                            string accNo = AddBankAccount(dal, ref ba);
+                            if (accNo == null)
                             {
-
-                                if (loopCount > 0)
-                                    Console.WriteLine("INVALID OPTION CHOSEN - PLEASE TRY AGAIN");
-
-                                Console.WriteLine("");
-                                Console.WriteLine("***Account Types***:");
-                                Console.WriteLine("1. Current Account.");
-                                Console.WriteLine("2. Savings Account.");
-                                Console.WriteLine("3. Back to Main Menu.");
-                                Console.WriteLine("CHOOSE OPTION:");
-                                accountType = Console.ReadLine();
-
-                                loopCount++;
-                            } while (!(accountType == AccountType.CurrentAccount || accountType == AccountType.SavingsAccount ||  accountType == SENTINEL));
-                            // Back to main menu
-                            if(accountType == SENTINEL)
-                            {
-                                break; 
+                                break; //  go back to the main menu
                             }
-
-                            String name = "";
-                            loopCount = 0;
-
-                            do
-                            {
-
-                                if (loopCount > 0)
-                                    Console.WriteLine("INVALID NAME ENTERED - PLEASE TRY AGAIN");
-
-                                Console.WriteLine("Enter Name: ");
-                                name = Console.ReadLine();
-
-                                loopCount++;
-
-                            } while (!IsValidName(name));
-
-                            String addressLine1 = "";
-                            loopCount = 0;
-
-                            do
-                            {
-
-                                if (loopCount > 0)
-                                    Console.WriteLine("INVALID ÀDDRESS LINE 1 ENTERED - PLEASE TRY AGAIN");
-
-                                Console.WriteLine("Enter Address Line 1: ");
-                                addressLine1 = Console.ReadLine();
-
-                                loopCount++;
-
-                            } while (addressLine1.Equals(""));
-
-                            Console.WriteLine("Enter Address Line 2: ");
-                            String addressLine2 = Console.ReadLine();
-
-                            Console.WriteLine("Enter Address Line 3: ");
-                            String addressLine3 = Console.ReadLine();
-
-                            String town = "";
-                            loopCount = 0;
-
-                            do
-                            {
-
-                                if (loopCount > 0)
-                                    Console.WriteLine("INVALID TOWN ENTERED - PLEASE TRY AGAIN");
-
-                                Console.WriteLine("Enter Town: ");
-                                town = Console.ReadLine();
-
-                                loopCount++;
-
-                            } while (town.Equals(""));
-
-                            double balance = -1;
-                            loopCount = 0;
-
-                            do
-                            {
-
-                                if (loopCount > 0)
-                                    Console.WriteLine("INVALID OPENING BALANCE ENTERED - PLEASE TRY AGAIN");
-
-                                Console.WriteLine("Enter Opening Balance: ");
-                                String balanceString = Console.ReadLine();
-
-                                try
-                                {
-                                    balance = Convert.ToDouble(balanceString);
-                                }
-
-                                catch
-                                {
-                                    loopCount++;
-                                }
-
-                            } while (balance < 0);
-
-                            Bank_Account ba;
-
-                            if (Convert.ToInt32(accountType) == Account_Type.Current_Account)
-                            {
-                                double overdraftAmount = -1;
-                                loopCount = 0;
-
-                                do
-                                {
-
-                                    if (loopCount > 0)
-                                        Console.WriteLine("INVALID OVERDRAFT AMOUNT ENTERED - PLEASE TRY AGAIN");
-
-                                    Console.WriteLine("Enter Overdraft Amount: ");
-                                    String overdraftAmountString = Console.ReadLine();
-
-                                    try
-                                    {
-                                        overdraftAmount = Convert.ToDouble(overdraftAmountString);
-                                    }
-
-                                    catch
-                                    {
-                                        loopCount++;
-                                    }
-
-                                } while (overdraftAmount < 0);
-
-                                ba = new Current_Account(name, addressLine1, addressLine2, addressLine3, town, balance, overdraftAmount);
-                            }
-
-                            else
-                            {
-
-                                double interestRate = -1;
-                                loopCount = 0;
-
-                                do
-                                {
-
-                                    if (loopCount > 0)
-                                        Console.WriteLine("INVALID INTEREST RATE ENTERED - PLEASE TRY AGAIN");
-
-                                    Console.WriteLine("Enter Interest Rate: ");
-                                    String interestRateString = Console.ReadLine();
-
-                                    try
-                                    {
-                                        interestRate = Convert.ToDouble(interestRateString);
-                                    }
-
-                                    catch
-                                    {
-                                        loopCount++;
-                                    }
-
-                                } while (interestRate < 0);
-
-                                ba = new Savings_Account(name, addressLine1, addressLine2, addressLine3, town, balance, interestRate);
-                            }
-
-                            String accNo = dal.AddBankAccount(ba);
-
-                            Console.WriteLine("New Account Number Is: " + accNo);
-
-                            // Clean 
-                            accNo = null;
-                            GC.Collect();
-
 
                             break;
                         case "2":
@@ -255,9 +80,9 @@ namespace Banking_Application
 
                                 Bank_Account ba_details = dal.FindBankAccountByAccNo(accNo);
                                 Console.WriteLine(ba_details.ToString());
-                                
+
                                 // clean after use it 
-                                
+
                                 ba_details = null;
                                 GC.Collect();
 
@@ -306,7 +131,7 @@ namespace Banking_Application
                             else
                             {
                                 double amountToLodge = -1;
-                                loopCount = 0;
+                                int loopCount = 0; // Declare 'loopCount' here
 
                                 do
                                 {
@@ -347,7 +172,7 @@ namespace Banking_Application
                             {
 
                                 double amountToWithdraw = -1;
-                                loopCount = 0;
+                                int loopCount = 0; // Declare 'loopCount' here
 
                                 do
                                 {
@@ -397,11 +222,202 @@ namespace Banking_Application
 
 
         }
+        private static string GetMenu()
+        {
+            Console.WriteLine("");
+            Console.WriteLine("***Banking Application Menu***");
+            Console.WriteLine("1. Add Bank Account");
+            Console.WriteLine("2. Close Bank Account");
+            Console.WriteLine("3. View Account Information");
+            Console.WriteLine("4. Make Lodgement");
+            Console.WriteLine("5. Make Withdrawal");
+            Console.WriteLine("6. Exit");
+            Console.WriteLine("CHOOSE OPTION:");
+            String option = Console.ReadLine();
+            return option;
+        }
         private static bool IsValidName(string name)
         {
             Regex regex = new Regex("^[a-zA-Z]{3,}$");
 
             return regex.IsMatch(name);
         }
+        private static string AddBankAccount(Data_Access_Layer dal, ref Bank_Account ba)
+        {
+
+
+            int loopCount = 0;
+            string accountType = "";
+            const string SENTINEL = "3";
+
+            do
+            {
+
+                if (loopCount > 0)
+                    Console.WriteLine("INVALID OPTION CHOSEN - PLEASE TRY AGAIN");
+
+                Console.WriteLine("");
+                Console.WriteLine("***Account Types***:");
+                Console.WriteLine("1. Current Account.");
+                Console.WriteLine("2. Savings Account.");
+                Console.WriteLine("3. Back to Main Menu.");
+                Console.WriteLine("CHOOSE OPTION:");
+                accountType = Console.ReadLine();
+
+                loopCount++;
+            } while (!(accountType == AccountType.CurrentAccount || accountType == AccountType.SavingsAccount || accountType == SENTINEL));
+            // Back to main menu
+            if (accountType == SENTINEL)
+            {
+                return null; // Return null to indicate going back to the main menu
+            }
+
+            String name = "";
+            loopCount = 0;
+
+            do
+            {
+
+                if (loopCount > 0)
+                    Console.WriteLine("INVALID NAME ENTERED - PLEASE TRY AGAIN");
+
+                Console.WriteLine("Enter Name: ");
+                name = Console.ReadLine();
+
+                loopCount++;
+
+            } while (!IsValidName(name));
+
+            String addressLine1 = "";
+            loopCount = 0;
+
+            do
+            {
+
+                if (loopCount > 0)
+                    Console.WriteLine("INVALID ÀDDRESS LINE 1 ENTERED - PLEASE TRY AGAIN");
+
+                Console.WriteLine("Enter Address Line 1: ");
+                addressLine1 = Console.ReadLine();
+
+                loopCount++;
+
+            } while (addressLine1.Equals(""));
+
+            Console.WriteLine("Enter Address Line 2: ");
+            String addressLine2 = Console.ReadLine();
+
+            Console.WriteLine("Enter Address Line 3: ");
+            String addressLine3 = Console.ReadLine();
+
+            String town = "";
+            loopCount = 0;
+
+            do
+            {
+
+                if (loopCount > 0)
+                    Console.WriteLine("INVALID TOWN ENTERED - PLEASE TRY AGAIN");
+
+                Console.WriteLine("Enter Town: ");
+                town = Console.ReadLine();
+
+                loopCount++;
+
+            } while (!IsValidName(town));
+
+            double balance = -1;
+            loopCount = 0;
+
+            do
+            {
+
+                if (loopCount > 0)
+                    Console.WriteLine("INVALID OPENING BALANCE ENTERED - PLEASE TRY AGAIN");
+
+                Console.WriteLine("Enter Opening Balance: ");
+                String balanceString = Console.ReadLine();
+
+                try
+                {
+                    balance = Convert.ToDouble(balanceString);
+                }
+
+                catch
+                {
+                    loopCount++;
+                }
+
+            } while (balance < 0);
+
+            if (Convert.ToInt32(accountType) == Account_Type.Current_Account)
+            {
+                double overdraftAmount = -1;
+                loopCount = 0;
+
+                do
+                {
+
+                    if (loopCount > 0)
+                        Console.WriteLine("INVALID OVERDRAFT AMOUNT ENTERED - PLEASE TRY AGAIN");
+
+                    Console.WriteLine("Enter Overdraft Amount: ");
+                    String overdraftAmountString = Console.ReadLine();
+
+                    try
+                    {
+                        overdraftAmount = Convert.ToDouble(overdraftAmountString);
+                    }
+
+                    catch
+                    {
+                        loopCount++;
+                    }
+
+                } while (overdraftAmount < 0);
+
+                ba = new Current_Account(name, addressLine1, addressLine2, addressLine3, town, balance, overdraftAmount);
+            }
+
+            else
+            {
+
+                double interestRate = -1;
+                loopCount = 0;
+
+                do
+                {
+
+                    if (loopCount > 0)
+                        Console.WriteLine("INVALID INTEREST RATE ENTERED - PLEASE TRY AGAIN");
+
+                    Console.WriteLine("Enter Interest Rate: ");
+                    String interestRateString = Console.ReadLine();
+
+                    try
+                    {
+                        interestRate = Convert.ToDouble(interestRateString);
+                    }
+
+                    catch
+                    {
+                        loopCount++;
+                    }
+
+                } while (interestRate < 0);
+
+                ba = new Savings_Account(name, addressLine1, addressLine2, addressLine3, town, balance, interestRate);
+            }
+
+            String accNo = dal.AddBankAccount(ba);
+            Console.WriteLine("New Account Number Is: " + accNo);
+
+            // Clean 
+            accNo = null;
+            GC.Collect();
+            return accNo;
+
+        }
+
     }
 }
